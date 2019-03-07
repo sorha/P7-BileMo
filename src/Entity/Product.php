b7bc840eb4b66606bab66d54986253d83f732f87
Sorha <sorha.entropy@gmail.com>
2019-03-07 03:00:34 +0100

    Api Plateform and EasyAdmin fresh installation

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
  * @UniqueEntity("name")
  */
